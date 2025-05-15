2282e1698312d509ff945c908e9db032e0b4ce3c
umutuca <3tomu24.ucarum@folkuniversitetet.nu>
2025-05-15 10:02:07 +0200

    gitaction

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Här kan du lägga till anpassade frågor om det behövs
+
     User findByUsername(String username);
 }
